88fa0199515296ae9ef94ae941f3d26def3559fe
andresPalmaB <144746932+andresPalmaB@users.noreply.github.com>
2024-07-08 06:20:45 +0000

    actualizacion del curso terminado, falta por ressolver ejercicios propuestos

--- aprende_logica_programacion/programacion_JS3/app.js
+++ aprende_logica_programacion/programacion_JS3/app.js
@@ -1,8 +1,36 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+alert('Juego: Adivina el NÃºmero');
+// Variables
+let numeroUsuario = 0; // Valor introducido por el usuario
+let intentos = 1; // Se inicializa en 1 porque el usuario al menos va a jugar una vez
 
-console.log(numeroUsuario);
+// Se le pide al usuario el numero maximo con el que quiere jugar
+let numeroRangoMayor = parseInt(prompt('Indique entre 1 y que numero quiere jugar: '));
+let numeroSecreto = Math.floor(Math.random() * numeroRangoMayor) + 1; // Valor a encontrar por el usuario
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+// Se le pide al usuario el numero maximo de intentos con los que quiere jugar
+let maximoIntentos = parseInt(prompt('Indique el numero maximo de intentos con los que quiere jugar: '));
+
+// Instruccion necesaria para permitirle al usuario jugar mas de una vez
+while (numeroUsuario != numeroSecreto) {
+    // Se le pide al usuario el numero que debe adivinar
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroRangoMayor} por favor: `));
+
+    // Instruccion necesaria para saber cuando se gana el juego
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste el numero es ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
+    } else {
+        // Se le brinda al usuario una ayuda para que puedo dar con el numero en cuention
+        if (numeroUsuario < numeroSecreto){
+            alert('No hacertaste, el numero es mayor.');
+        } else {
+            alert('No hacertaste, el numero es menor.');
+        }
+        // Se incrementa el contador cuando no hay un acierto
+        intentos++;
+        //palabraVeces = 'veces';
+        if (intentos > maximoIntentos) {
+            alert(`Â¡Perdise! Haz superado el numero maximos de ${intentos} intentos. El numero a adivinar era ${numeroSecreto}.`)
+            break;
+        }
+    };
 }
---
+++ aprende_logica_programacion/programacion_JS3/ejercicios_propuestos1.js
@@ -0,0 +1,45 @@
+//ejercicio 1
+/*Muestra una alerta con el mensaje
+"¡Bienvenida y bienvenido a nuestro sitio web!".*/
+alert(' ¡Bienvenida y bienvenido a nuestro sitio web! ');
+
+/*ejercicio 2
+declara una varible llamada nombre y asignale el valor "luna" */
+//let nombre = 'luna';
+
+/*ejercicio 3
+crea una variable llamada edad y asignale el valor 25 */
+//let edad = 25;
+
+/*ejercicio 4
+establece una varible numeroDeVentas y asignale el valor 50 */
+//let numeroDeVentas = 50;
+
+/*ejercicio 5
+establece una variable saldoDisponible y asignale el valor 1000*/
+//let saldoDisponible = 1000;
+
+/*ejercicio 6
+muestra una alerta con el texto "¡Error! Completa todos los campos" */
+//alert('¡Error! Completa todos los campos');
+
+/*ejercicio 7
+declara una variable llamada mensajeDeError y asignale el valor "¡Error! completa todos los campos"
+ahora muestra una alerta con el valor de la variable mensajeDeError */
+//let mensajeDeError = '¡Error! Completa todos los campos'
+//alert(mensajeDeError);
+
+/*ejercicio 8
+Utiliza un prompt para preguntar el nombre de usuario y almacenalo en la variable nombre */
+//let nombre = prompt('Digite su nombre de usuario: ');
+
+/*ejercicio 9
+pide al usuario que ingrese su edad usando un prompt y almacenala en la variable edad */
+let edad = prompt('Digite su edad: ');
+
+/*ejercicio 10
+ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje '¡puedes obtener tu licencia de conducir!' */
+
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!')
+}
